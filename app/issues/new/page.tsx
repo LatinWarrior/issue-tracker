0c34e4be048e48090f51d21e05e36d901f588898
git <luis.f.blanco@gmail.com>
2023-10-15 14:22:27 -0500

    Implement client-side validation.

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Button, Callout, TextField } from '@radix-ui/themes';
+import { Button, Callout, Text, TextField } from '@radix-ui/themes';
 import SimpleMDE from 'react-simplemde-editor';
 import { useForm, Controller } from 'react-hook-form';
 import axios from 'axios';
@@ -8,15 +8,27 @@ import { useRouter } from 'next/navigation';
 
 import 'easymde/dist/easymde.min.css';
 import { useState } from 'react';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { createIssueSchema } from '@/app/validationSchema';
+import { z } from 'zod';
 
-interface IssueForm {
-    title: string;
-    description: string;
-}
+type IssueForm = z.infer<typeof createIssueSchema>;
+
+// interface IssueForm {
+//     title: string;
+//     description: string;
+// }
 
 const NewIssuePage = () => {
     const router = useRouter();
-    const { register, control, handleSubmit } = useForm<IssueForm>();
+    const {
+        register,
+        control,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<IssueForm>({
+        resolver: zodResolver(createIssueSchema),
+    });
     const [error, setError] = useState('');
     // console.log(register('title'));
 
@@ -43,6 +55,11 @@ const NewIssuePage = () => {
                         {...register('title')}
                     />
                 </TextField.Root>
+                {errors.title && (
+                    <Text color='red' as='p'>
+                        {errors.title.message}
+                    </Text>
+                )}
                 <Controller
                     name='description'
                     control={control}
@@ -50,6 +67,11 @@ const NewIssuePage = () => {
                         <SimpleMDE placeholder='Description' {...field} />
                     )}
                 />
+                {errors.description && (
+                    <Text color='red' as='p'>
+                        {errors.description.message}
+                    </Text>
+                )}
                 <Button>Submit New Issue</Button>
             </form>
         </div>
